323327ceef3a9339ab1d2aa5b7fb6517f2aa0e4e
Daniel Miolan <miolan96@gmail.com>
2022-01-18 19:05:28 -0400

    Create project.

---
+++ Lab1_DanielMiolan_c0817504_iOS/Lab1_DanielMiolan_c0817504_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab1_DanielMiolan_c0817504_iOS
+//
+//  Created by Daniel Miolan on 1/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
