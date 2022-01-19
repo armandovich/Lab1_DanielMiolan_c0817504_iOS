cbf2a69235e1a249bcbd596fbfe24cce6ec293d0
Daniel Miolan <miolan96@gmail.com>
2022-01-18 20:08:24 -0400

    Create outlets and actions.

--- Lab1_DanielMiolan_c0817504_iOS/Lab1_DanielMiolan_c0817504_iOS/ViewController.swift
+++ Lab1_DanielMiolan_c0817504_iOS/Lab1_DanielMiolan_c0817504_iOS/ViewController.swift
@@ -9,12 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet var buttonList: Array<UIButton>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        for btn in buttonList {
+            print("We have Tag #\(btn.tag)")
+        }
     }
 
+    @IBAction func PlayGame(_ sender: UIButton) {
+        print("Start Game")
+    }
     
+    @IBAction func SelectPosition(_ sender: UIButton) {
+        print("Tap on Button #\(sender.tag)")
+    }
 }
 
